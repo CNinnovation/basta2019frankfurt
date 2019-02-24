767c2eb13d3707e652e25527c9519b8dfc14bb7a
Christian Nagel <christian@christiannagel.com>
2019-02-24 15:27:40 +0100

    using declaration

---
+++ CSharp8/UsingDeclaraationSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsingDeclaraationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run1();
+            Console.WriteLine("fin");
+        }
+
+        static void Run1()
+        {
+            {
+                using var r = new Resource();
+                r.Foo();
+            }
+            Console.WriteLine("next scope");
+        }
+
+    }
+}
---
+++ CSharp8/UsingDeclaraationSample/Resource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingDeclaraationSample
+{
+    public class Resource // : IDisposable
+    {
+        public void Foo() => Console.WriteLine("Resource.Foo");
+        public void Dispose() => Console.WriteLine("Resource.Dispose");
+    }
+}
