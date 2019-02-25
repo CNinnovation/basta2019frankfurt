badb0fa589540dc19bcbc9ac58abf8a9c1214ffc
Christian Nagel <christian@christiannagel.com>
2019-02-25 19:13:37 +0100

    cleanup using declarations

--- CSharp8/UsingDeclarationSample/Program.cs
+++ CSharp8/UsingDeclarationSample/Program.cs
@@ -8,10 +8,18 @@ namespace UsingDeclaraationSample
         {
             Run1();
             Run2();
+            Run3();
             Console.WriteLine("fin");
         }
 
         static void Run1()
+        {
+            using var r = new Resource();
+            r.Foo();
+            Console.WriteLine("end Run1");
+        }
+
+        static void Run2()
         {
             {
                 using var r = new Resource();
@@ -20,7 +28,7 @@ namespace UsingDeclaraationSample
             Console.WriteLine("next scope");
         }
 
-        static void Run2()
+        static void Run3()
         {
             {
                 using var r = new Resource2();
