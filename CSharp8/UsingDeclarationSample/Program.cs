9b8deaaaa3b3a4cb519f77491d9566bde2d91d24
Christian Nagel <christian@christiannagel.com>
2019-02-25 17:57:51 +0100

    using declaration and pattern

--- CSharp8/UsingDeclarationSample/Program.cs
+++ CSharp8/UsingDeclarationSample/Program.cs
@@ -4,13 +4,23 @@ namespace UsingDeclaraationSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Run1();
+            Run2();
             Console.WriteLine("fin");
         }
 
         static void Run1()
+        {
+            {
+                using var r = new Resource();
+                r.Foo();
+            }
+            Console.WriteLine("next scope");
+        }
+
+        static void Run2()
         {
             {
                 using var r = new Resource2();
--- CSharp8/UsingDeclarationSample/Resource.cs
+++ CSharp8/UsingDeclarationSample/Resource.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace UsingDeclaraationSample
 {
-    public class Resource // : IDisposable
+    public class Resource : IDisposable
     {
         public void Foo() => Console.WriteLine("Resource.Foo");
         public void Dispose() => Console.WriteLine("Resource.Dispose");
@@ -15,6 +15,8 @@ namespace UsingDeclaraationSample
         public Resource2(int value1) => Value1 = value1;
 
         public int Value1 { get; }
+
+        public void Foo() => Console.WriteLine("Resource2.Foo");
 
         public void Dispose() => Console.WriteLine("Resource2.Dipose");
     }
