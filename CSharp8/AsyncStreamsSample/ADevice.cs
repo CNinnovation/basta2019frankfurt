01a32ce86ec87d7007414d08cafd0fe074f048f2
Christian Nagel <christian@christiannagel.com>
2019-02-26 08:31:03 +0100

    cleanup async

--- CSharp8/AsyncStreamsSample/ADevice.cs
+++ CSharp8/AsyncStreamsSample/ADevice.cs
@@ -23,7 +23,6 @@ namespace AsyncStreamsSample
                     };
                 }
                 Console.WriteLine("cancel requested");
-                yield break;
             };
 
         }
--- CSharp8/AsyncStreamsSample/SensorData.cs
+++ CSharp8/AsyncStreamsSample/SensorData.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AsyncStreamsSample
+﻿namespace AsyncStreamsSample
 {
     public class SensorData
     {
