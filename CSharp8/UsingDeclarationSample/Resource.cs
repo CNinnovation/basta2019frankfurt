4f76db8dcce71824f51663abd783366e9758c21e
Christian Nagel <christian@christiannagel.com>
2019-02-26 09:27:00 +0100

    using declaration extended

--- CSharp8/UsingDeclarationSample/Resource.cs
+++ CSharp8/UsingDeclarationSample/Resource.cs
@@ -7,15 +7,4 @@ namespace UsingDeclarationSample
         public void Foo() => Console.WriteLine("Resource.Foo");
         public void Dispose() => Console.WriteLine("Resource.Dispose");
     }
-
-    public ref struct Resource2 // : IDisposable - ref structs cannot imlpement interfaces
-    {
-        public Resource2(int value1) => Value1 = value1;
-
-        public int Value1 { get; }
-
-        public void Foo() => Console.WriteLine("Resource2.Foo");
-
-        public void Dispose() => Console.WriteLine("Resource2.Dipose");
-    }
 }
---
+++ CSharp8/UsingDeclarationSample/Resource2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingDeclarationSample
+{
+    public ref struct Resource2 // : IDisposable - ref structs cannot imlpement interfaces
+    {
+        public Resource2(int value1) => Value1 = value1;
+
+        public int Value1 { get; }
+
+        public void Foo() => Console.WriteLine("Resource2.Foo");
+
+        public void Dispose() => Console.WriteLine("Resource2.Dipose");
+    }
+}
